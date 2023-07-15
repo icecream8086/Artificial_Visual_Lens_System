db59d32fee580272274d48ecc90ff0a2fc31acfa
icecream8086 <icecream8086@outlook.com>
2023-07-15 19:48:41 +0800

    1

---
+++ Back-end/Visual_Lens_Server/model_test.py
@@ -0,0 +1,76 @@
+import torch
+from torchvision import datasets
+import torch.nn as nn
+from CNN_lib.net_model import ResNet_0602
+from CNN_lib.dataset_sample import transform
+
+
+train_set = datasets.ImageFolder('dataset', transform=transform)
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=32, shuffle=True)
+
+# 划分数据集
+train_size = int(len(train_set) * 0.6)
+test_size = int(len(train_set) * 0.2)
+val_size = len(train_set) - train_size - test_size
+train_set, test_set, val_set = torch.utils.data.random_split(train_set, [train_size, test_size, val_size])
+
+# 加载数据集
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=32, shuffle=True)
+test_loader = torch.utils.data.DataLoader(test_set, batch_size=32, shuffle=True)
+val_loader = torch.utils.data.DataLoader(val_set, batch_size=32, shuffle=True)
+
+# 判断是否有GPU可用
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print('Device:', device)
+
+# 加载模型到GPU
+model = ResNet_0602(num_classes=5).to(device)
+
+# 加载模型状态字典到CPU
+state_dict = torch.load('ResNet-0602-pruned.pth', map_location=torch.device('cpu'))
+
+# 将状态字典中的 fc 层调整为 [5, 512]
+state_dict['fc.3.weight'] = state_dict['fc.3.weight'][:5]
+state_dict['fc.3.bias'] = state_dict['fc.3.bias'][:5]
+
+# 将状态字典加载到模型对象中
+model.load_state_dict(state_dict, strict=False)
+
+# 将模型设置为评估模式
+model.eval()
+
+# 将数据移动到GPU
+val_loss = torch.tensor(0.0, dtype=torch.float32, device=device)
+val_acc = torch.tensor(0.0, dtype=torch.float32, device=device)
+test_loss = torch.tensor(0.0, dtype=torch.float32, device=device)
+test_acc = torch.tensor(0.0, dtype=torch.float32, device=device)
+
+# 在验证集上评估模型
+with torch.no_grad():
+    for images, labels in val_loader:
+        criterion = nn.CrossEntropyLoss()
+        images, labels = images.to(device), labels.to(device)
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        val_loss += loss.item()
+        _, predicted = torch.max(outputs.data, 1)
+        val_acc += (predicted == labels).sum().item()
+
+# 在测试集上评估模型
+with torch.no_grad():
+    for images, labels in test_loader:
+        images, labels = images.to(device), labels.to(device)
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        test_loss += loss.item()
+        _, predicted = torch.max(outputs.data, 1)
+        test_acc += (predicted == labels).sum().item()
+
+# 计算平均损失和准确率
+val_loss /= len(val_loader.dataset)
+val_acc /= len(val_loader.dataset)
+test_loss /= len(test_loader.dataset)
+test_acc /= len(test_loader.dataset)
+
+print('Validation Loss: {:.4f}, Validation Accuracy: {:.4f}'.format(val_loss.item(), val_acc.item()))
+print('Test Loss: {:.4f}, Test Accuracy: {:.4f}'.format(test_loss.item(), test_acc.item()))
---
+++ Back-end/Visual_Lens_Server/train.py
@@ -0,0 +1,93 @@
+import logging
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets
+from torch.optim.lr_scheduler import StepLR
+from CNN_lib.dataset_normal import transform
+from CNN_lib.net_model import ResNet_0602
+
+train_set_1 = datasets.ImageFolder('dataset', transform=transform)
+basicset = torch.utils.data.ConcatDataset([train_set_1, ])
+basicloader = torch.utils.data.DataLoader(basicset, batch_size=32, shuffle=True)
+indices = torch.randperm(len(basicset))
+basicset = torch.utils.data.Subset(basicset, indices)
+train_size = int(len(basicset) * 0.6)
+test_size = int(len(basicset) * 0.2)
+val_size = len(basicset) - train_size - test_size
+train_set, test_set, val_set = torch.utils.data.random_split(basicset, [train_size, test_size, val_size])
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=32, shuffle=True)
+test_loader = torch.utils.data.DataLoader(test_set, batch_size=32, shuffle=True)
+val_loader = torch.utils.data.DataLoader(val_set, batch_size=32, shuffle=True)
+
+# 定义模型
+
+
+# 定义训练函数
+# 设置日志文件路径和格式
+logging.basicConfig(filename='train.log', level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s')
+
+# 在 train 函数中添加日志输出
+def train(model, device, train_loader, criterion, optimizer, epoch):
+    model.train()
+    running_loss = 0.0
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data = data.float()  # 将数据转换为浮点数类型
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = criterion(output, target)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        if batch_idx % 10 == 9:
+            # 使用 print 函数输出原来的信息
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), running_loss / 10))
+            # 使用 logging 模块输出日志信息
+            logging.info('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), running_loss / 10))
+            running_loss = 0.0
+
+# 在 test 函数中添加日志输出
+def test(model, device, val_loader, criterion):
+    model.eval()
+    val_loss = 0.0
+    correct = 0
+    with torch.no_grad():
+        for data, target in val_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            val_loss += criterion(output, target).item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    val_loss /= len(val_loader.dataset)
+
+    # 使用 print 函数输出原来的信息
+    print('\nValidation set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        val_loss, correct, len(val_loader.dataset),
+        100. * correct / len(val_loader.dataset)))
+    # 使用 logging 模块输出日志信息
+    logging.info('\nValidation set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        val_loss, correct, len(val_loader.dataset),
+        100. * correct / len(val_loader.dataset)))
+
+# 设置超参数并训练模型
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = ResNet_0602(num_classes=10)
+model.to(device)
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+scheduler = StepLR(optimizer, step_size=10, gamma=0.1)  # 定义学习率调度器
+criterion = nn.CrossEntropyLoss()
+epochs = 360
+
+for epoch in range(1, epochs + 1):
+    train(model, device, train_loader, criterion, optimizer, epoch)
+    test(model, device, val_loader, criterion)
+    scheduler.step()  # 每个epoch结束后调用学习率调度器进行自我学习率调整
+    
+torch.save(model.state_dict(), 'ResNet-0602.pth')
