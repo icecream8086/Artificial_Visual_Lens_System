3e5df913da7fecfa9b5ab5b237c43693df42a61e
icecream8086 <icecream8086@outlook.com>
2023-07-30 01:54:01 +0800

    file upload module test

--- Back-end/image_management_Server/routes/users.js
+++ Back-end/image_management_Server/routes/users.js
@@ -1,7 +1,9 @@
+// @ts-nocheck
 var express = require('express');
 var router = express.Router();
 var db = require('../lib/datasource/mysql_connection');  // 引用数据库连接
-
+const fs = require('fs');
+const multer = require('multer');
 /* this is a demo page */
 
 // GET /users
@@ -30,5 +32,51 @@ router.get('/select_test', function(req, res, next) {
 
   });
 });
+
+router.get('/image_test_small', function(req, res, next) {
+  // path: /public/images/small
+  res.sendFile('Small.jpg', { root: './File_Stream/Test' });
+});
+
+
+router.get('/image_test_Large', function(req, res, next) {
+  // path: /public/images/small
+  res.sendFile('Large.jpg', { root: './File_Stream/Test' });
+});
+
+
+
+router.get('/image_tests', function(req, res, next) {
+  //return sha256('file') 
+  res.sendFile('mfpiH90vynVo1qAi.png', { root: './File_Stream/Test/catch' });
+  local = req.headers.host;
+  return res.json({ message: 'image_test' , Image_URL: 'http://'+local+'/users/image_test' });
+});
+router.get('/image_test', function(req, res, next) {
+  //return sha256('file') 
+  res.sendFile('eager-for-power.jpg', { root: './File_Stream/Test/catch' });
+});
+
+
+
+
+// 配置multer
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './File_Stream/Test/catch'); // 设置文件保存的路径
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname); // 设置文件保存时的文件名
+  }
+});
+
+const upload = multer({ storage: storage });
+
+router.post('/upload_image_Test', upload.single('files'), function(req, res, next) {
+  // 文件上传成功后的处理逻辑
+  console.log('File uploaded successfully.');
+  res.status(200).send('File uploaded successfully.');
+});
+
 
 module.exports = router;
