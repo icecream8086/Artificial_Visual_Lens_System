de156aba360e31603a7e4b4f91b46e20b61d2245
LinLai <zzbsn.cn@outlook.com>
2023-08-01 19:37:19 +0800

    2

--- Front-end/Main_project/UnitTest/image_speedTest.py
+++ Front-end/Main_project/UnitTest/image_speedTest.py
@@ -18,7 +18,6 @@ with open("temp_image.jpg", "wb") as file:
 # 使用 PIL 库打开图像
 image = Image.open("temp_image.jpg")
 file_read = open("./temp_image.jpg", "rb")
-file_read.close()
 
 # 获取图像的分辨率和大小
 # get sha256 code of image
@@ -26,6 +25,7 @@ sha_256_code = hashlib.sha256(file_read.read())
 
 resolution = image.size
 filesize = len(response.content)
+file_read.close()
 
 # 打印结果
 print("Response Time:", response_time, "seconds")
@@ -33,4 +33,3 @@ print("Resolution:", resolution)
 print("File Size:", filesize , "bytes")
 print("File Size:", filesize / 1024 / 1024, "MB")
 print("Hash Code:", sha_256_code.hexdigest())
-os.remove("./temp_image.jpg")
