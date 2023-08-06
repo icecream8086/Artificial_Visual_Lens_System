9c8b430424c6d7f65140f865dd7ac850374c9a93
LAi Lin <60541369+icecream8086@users.noreply.github.com>
2023-08-06 18:24:17 +0800

    Update trigger.sql

--- blueprint/sql/image_management_Server/trigger.sql
+++ blueprint/sql/image_management_Server/trigger.sql
@@ -1,9 +1,14 @@
 /* useage = "触发器" */
+DELIMITER //
+
 CREATE TRIGGER user_insert AFTER INSERT ON users
 FOR EACH ROW
 BEGIN
   INSERT INTO auth_info (UID) VALUES (NEW.UID);
   INSERT INTO banned_users (UID) VALUES (NEW.UID);
   INSERT INTO user_access_info (UID) VALUES (NEW.UID);
-END;
+END //
+
+DELIMITER ;
+
 /* end */
