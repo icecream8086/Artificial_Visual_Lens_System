5de941ae8e952b9e99a0a135f01203e9d74e0f1b
icecream8086 <icecream8086@outlook.com>
2023-09-05 11:17:21 +0800

    2c

---
+++ Back-end/Visual_Lens_Server/CNN_lib/data_split.py
@@ -0,0 +1,19 @@
+import torch
+from torchvision import datasets
+from CNN_lib.dataset_sample import transform
+
+def data_split(path='dataset',transform=transform,train_rate=0.6,test_rate=0.2):
+    train_set = datasets.ImageFolder(path, transform=transform)
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=32, shuffle=True)
+
+# 划分数据集
+    train_size = int(len(train_set) * train_rate)
+    test_size = int(len(train_set) * test_rate)
+    val_size = len(train_set) - train_size - test_size
+    train_set, test_set, val_set = torch.utils.data.random_split(train_set, [train_size, test_size, val_size])
+
+# 加载数据集
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=32, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(test_set, batch_size=32, shuffle=True)
+    val_loader = torch.utils.data.DataLoader(val_set, batch_size=32, shuffle=True)
+    return test_loader,val_loader
--- Back-end/Visual_Lens_Server/train.py
+++ Back-end/Visual_Lens_Server/train.py
@@ -2,30 +2,19 @@ import logging
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torchvision import datasets
 from torch.optim.lr_scheduler import StepLR
 from CNN_lib.dataset_normal import transform
 from CNN_lib.net_model import ResNet_50_Customize
+from CNN_lib.data_split import data_split
 from tqdm import tqdm
 import json
 
 
-train_set_1 = datasets.ImageFolder('dataset', transform=transform)
-basicset = torch.utils.data.ConcatDataset([train_set_1, ])
-basicloader = torch.utils.data.DataLoader(basicset, batch_size=32, shuffle=True)
 
-indices = torch.randperm(len(basicset))
-basicset = torch.utils.data.Subset(basicset, indices)
-train_size = int(len(basicset) * 0.6)
-test_size = int(len(basicset) * 0.2)
-val_size = len(basicset) - train_size - test_size
-train_set, test_set, val_set = torch.utils.data.random_split(basicset, [train_size, test_size, val_size])
-train_loader = torch.utils.data.DataLoader(train_set, batch_size=32, shuffle=True)
-test_loader = torch.utils.data.DataLoader(test_set, batch_size=32, shuffle=True)
-val_loader = torch.utils.data.DataLoader(val_set, batch_size=32, shuffle=True)
+val_loader, train_loader = data_split(path='dataset', transform=transform, train_rate=0.6, test_rate=0.2)
 
-# 定义模型
-
+train_bar = tqdm(train_loader)  # 使用tqdm包装train_loader
+val_bar = tqdm(val_loader)  # 使用tqdm包装val_loader
 
 # 定义训练函数
 # 设置日志文件路径和格式
@@ -82,23 +71,21 @@ def test(model, device, val_loader, criterion):
 
 
 # 设置超参数并训练模型
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = ResNet_50_Customize(num_classes=10)
-model.to(device)
-optimizer = optim.Adam(model.parameters(), lr=0.001)
-scheduler = StepLR(optimizer, step_size=10, gamma=0.1)  # 定义学习率调度器
-criterion = nn.CrossEntropyLoss()
-epochs = 360
-
-train_bar = tqdm(train_loader)  # 使用tqdm包装train_loader
-val_bar = tqdm(val_loader)  # 使用tqdm包装val_loader
-
-for epoch in range(1, epochs + 1):
-    train(model, device, train_loader, criterion, optimizer, epoch)
-    test(model, device, val_loader, criterion)
-    scheduler.step()  # 每个epoch结束后调用学习率调度器进行自我学习率调整
+def train_module(val_loader, train_loader, train, test ,module_name='ResNet-0602.pth'):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = ResNet_50_Customize(num_classes=10)
+    model.to(device)
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    scheduler = StepLR(optimizer, step_size=10, gamma=0.1)  # 定义学习率调度器
+    criterion = nn.CrossEntropyLoss()
+    epochs = 360
+    for epoch in range(1, epochs + 1):
+        train(model, device, train_loader, criterion, optimizer, epoch)
+        test(model, device, val_loader, criterion)
+        scheduler.step()  # 每个epoch结束后调用学习率调度器进行自我学习率调整
     
-train_bar.close()  # 停止和清除进度条
-val_bar.close()
+    train_bar.close()  # 停止和清除进度条
+    val_bar.close()
+    torch.save(model.state_dict(), module_name)
 
-torch.save(model.state_dict(), 'ResNet-0602.pth')
+train_module(val_loader, train_loader, train, test)
