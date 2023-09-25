6d6d4e219bf41aa5d22b3c7b15918d7e2e9d5515
icecream8086 <icecream8086@outlook.com>
2023-09-25 09:45:32 +0800

    add global Task

--- Back-end/Visual_Lens_Server/web_server/app.py
+++ Back-end/Visual_Lens_Server/web_server/app.py
@@ -1,9 +1,9 @@
 from flask import Flask
 from router import test_module
-
+from health_check import health_check_bp
 app = Flask(__name__)
 
 app.register_blueprint(test_module)
-
+app.register_blueprint(health_check_bp)
 if __name__ == '__main__':
     app.run( host='0.0.0.0', port=5000, debug=True)
