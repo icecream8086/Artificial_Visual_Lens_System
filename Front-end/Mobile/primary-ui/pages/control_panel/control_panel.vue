18e16f11d8b7d3ab4f495bb78329980897d53887
LinLai <zzbsn.cn@outlook.com>
2023-10-08 12:25:17 +0800

    on lodad

--- Front-end/Mobile/primary-ui/pages/control_panel/control_panel.vue
+++ Front-end/Mobile/primary-ui/pages/control_panel/control_panel.vue
@@ -1,24 +1,53 @@
 <template>
   <view class="login_bg_containe">
-    <view class="content" style="overflow: auto">
-      <u-image :show-loading="true" :src="src" class="main_imagebox"></u-image>
-    </view>
-    <view class="bg_box_control_panel">
-      <view>
+    <view>
+      <div>
+        <u-row>
+          <u-col :span="6">
+            <image class="logo_big_icon" src="../../static/neural_network.png"></image>
+          </u-col>
+          <u-col :span="6">
+            <image class="logo_big_icon" src="../../static/neural_network.png"></image>
+          </u-col>
+        </u-row>
+      </div>
+      <div>
+        <u-search shape="square" :showAction="true" actionText="搜索" :animation="true"></u-search>
+      </div>
+      <view class="container_test">
         <div>
-          <div v-for="i in 100" :key="i" style="color: aqua">
-            <p>这是第 {{ i }} 段文本。</p>
-          </div>
+          <p>
+            <Progress_card :steps="1" :Statu_code="1" :data_time_train="'2023年10月8日10点17分'"
+              :data_time_Test="'2023年10月8日10点17分'" :data_time_relase="'2023年10月8日10点29分'"></Progress_card>
+          </p>
+          <p>
+            <Progress_card :steps="1" :Statu_code="1" :data_time_train="'2023年10月8日10点17分'"
+              :data_time_Test="'2023年10月8日10点17分'" :data_time_relase="'2023年10月8日10点29分'"></Progress_card>
+          </p>
         </div>
+        
+        <view>
+          <div>
+            <div v-for="i in 100" :key="i" style="color: aqua">
+              <p>这是第 {{ i }} 段文本。</p>
+            </div>
+          </div>
+
+        </view>
       </view>
     </view>
   </view>
 </template>
   
   
-  <script>
+<script>
+import uCol from '../../uni_modules/uview-ui/components/u-col/u-col.vue';
+import Progress_card from '../component/Progress_card.vue';
+
 var _this;
 export default {
+  name: "control_panel",
+  components: { uCol, Progress_card },
   data() {
     return {
       src: "https://cdn.uviewui.com/uview/album/1.jpg",
@@ -36,12 +65,7 @@ export default {
 };
 </script>
   
-  <style>
-
-
-
-
-
+<style>
 .login_bg_containe {
   background-image: url("../../static/bokeh-hex.jpg");
   background-size: cover;
@@ -74,6 +98,7 @@ export default {
   overflow: auto;
   z-index: 1;
 }
+
 .main_imagebox {
   min-width: 192px;
   min-height: 108px;
@@ -86,4 +111,20 @@ export default {
   border: #0881d2 3px solid;
   z-index: 2;
 }
-</style>
+
+.logo_big_icon {
+  height: 350rpx;
+  width: 350rpx;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 50rpx;
+}
+
+.container_test {
+
+  min-height: 400px;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: red;
+}</style>
